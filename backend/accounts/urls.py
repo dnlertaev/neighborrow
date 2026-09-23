0a63d119f3784f35c77c777deb1e87915da0bdd0
dnlertaev <157004121+dnlertaev@users.noreply.github.com>
2026-09-24 01:56:07 +0500

    Add backend/accounts/urls.py

---
+++ backend/accounts/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from rest_framework_simplejwt.views import TokenRefreshView
+from .views import LoginView, LogoutView, MeView, RegisterView, VerifyEmailView
+
+urlpatterns = [
+    path('register', RegisterView.as_view()), path('verify-email', VerifyEmailView.as_view()),
+    path('login', LoginView.as_view()), path('refresh', TokenRefreshView.as_view()),
+    path('logout', LogoutView.as_view()), path('me', MeView.as_view()),
+]
